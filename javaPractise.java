25964ccc8f0c543f9d5496ce0aa237cbe6d6415a
Mohit Khandelwal <mohitkhandelwal9784@gmail.com>
2023-09-21 23:29:02 +0530

    third commit

---
+++ javaPractise.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+
+
+class javaPractise{
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+    //     int n;
+    //     n=sc.nextInt();
+    //    int arr[]=new int[n];
+    //    for(int i=0;i<n;i++){
+    //     arr[i]=sc.nextInt();
+    //    }
+
+
+
+    //    Arrays.sort(arr);
+    //    Arrays.sort(arr,Collections.reverseOrder());
+       
+
+    //    for(int i=0;i<n;i++){
+    //     System.out.print(arr[i]+" ");
+
+    //    }System.out.println();
+
+    //    String s;
+    //    s=sc.nextLine();
+    //    System.out.println(s);
+
+    //    System.out.println(s.length());
+
+    // set
+    // Set<String> hash_Set = new HashSet<String>();
+
+    // for(int i=0;i<10;i++){
+    //     hash_Set.add("hello"+i);
+    // }
+    // for(int i=0;i<10;i++){
+    //     hash_Set.add("hello"+i);
+    // }
+
+    // for(String it:hash_Set){
+    //     System.out.println(it);
+
+    // }
+
+
+    // Set<Integer> mySet=new HashSet<Integer>();
+
+    // for(int i=0;i<10;i++){
+    //     mySet.add(i);
+    // }
+
+    // for(int i=0;i<10;i++){
+    //     mySet.add(i);
+    // }
+
+
+    // for(int it:mySet){
+    //     System.out.println(it);
+    // }
+
+    // System.out.print(mySet.contains(20));
+
+
+    // Map<Integer,Integer>ump=new HashMap<Integer, Integer>();
+
+    // for(int i=0;i<=10;i++){
+    //     ump.put(i,i+1);
+    // }
+
+    // for(Map.Entry<Integer,Integer>it:ump.entrySet()){
+    //     System.out.println(it.getKey()+" "+it.getValue());
+
+    // }
+
+    // Map<String,Integer>ump1=new HashMap<String,Integer>();
+    
+    //     ump1.put("a",1);
+    //     ump1.put("b",2);
+    //     ump1.put("c",3);
+    //     ump1.put("d",4);
+
+    //     System.out.println(ump1.get("a"));
+
+    //     for(Map.Entry<String,Integer>it:ump1.entrySet()){
+    //         System.out.println(it.getKey()+" "+it.getValue());
+
+
+    //     }
+
+    //     System.out.println(ump1.containsKey("Z"));
+
+
+    // ArrayList
+
+    // ArrayList<Integer>arr=new ArrayList<Integer>();
+
+    // for(int i=0;i<10;i++){
+    //     arr.add(i);
+    // }
+
+    // for(int it:arr){
+    //     System.out.println(it);
+    // }
+
+    }
+    
+
+};
